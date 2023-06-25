11eeca15da2d3fab6a2f7702ef0602f49848c98e
hipolito <huaylla7@hotmail.com>
2023-06-24 21:06:21 -0400

    Diseño terminado

---
+++ app/src/main/java/org/matematicasfacil/actividad9/MainActivity.java
@@ -0,0 +1,31 @@
+package org.matematicasfacil.actividad9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView otono = findViewById(R.id.imageViewOto);
+        ImageView invierno = findViewById(R.id.imageViewInvi);
+        ImageView primavera = findViewById(R.id.imageViewPrim);
+        ImageView verano = findViewById(R.id.imageViewVera);
+
+        otono.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent secondActivity = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(secondActivity);
+            }
+        });
+
+    }
+}
