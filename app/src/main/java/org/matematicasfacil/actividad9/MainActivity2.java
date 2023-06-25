f65e5521cd0073dcdad10f2b4db207633437e5b2
hipolito <huaylla7@hotmail.com>
2023-06-25 00:09:03 -0400

    Imagen seleccionada y animada.

--- app/src/main/java/org/matematicasfacil/actividad9/MainActivity2.java
+++ app/src/main/java/org/matematicasfacil/actividad9/MainActivity2.java
@@ -2,21 +2,26 @@ package org.matematicasfacil.actividad9;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
 import android.view.animation.ScaleAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    ImageView recibeImagen;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        ImageView recibeImagen = findViewById(R.id.imageViewOto);
+        recibeImagen = findViewById(R.id.imageViewSeleccionada);
         Button btnVolver = findViewById(R.id.buttonVolver);
 
         Intent intent = getIntent();
@@ -24,6 +29,7 @@ public class MainActivity2 extends AppCompatActivity {
             int selectedImageId = intent.getIntExtra("clave", 0);
             recibeImagen.setImageResource(selectedImageId);
         }
+        animarImagen();
 
         btnVolver.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,5 +39,14 @@ public class MainActivity2 extends AppCompatActivity {
         });
     }
 
+    private void animarImagen() {
+        ObjectAnimator escalaEnX = ObjectAnimator.ofFloat(recibeImagen, View.SCALE_X, 1f, 2f);//1f escala original
+        ObjectAnimator escalaEnY = ObjectAnimator.ofFloat(recibeImagen, View.SCALE_Y, 1f, 2f);//2f escala final
 
+        escalaEnX.setDuration(2000); // Duración de la animación en milisegundos
+        escalaEnY.setDuration(2000);
+
+        escalaEnX.start();
+        escalaEnY.start();
+    }
 }
